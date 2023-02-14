ed2c7114a86ae1f03346ca7a77f3e5678687d4d9
Derick Dspain <derickdspain@yahoo.com>
2023-02-13 23:19:40 -0600

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,17 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let passwordLength = prompt("How many characters? 8-128");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    let nums = prompt("Include Numbers?");
+    let specialCharacters = prompt("Include Special Characters?");
+    let lowerCase = prompt("Include Lowercase?");
+    let upperCase = prompt("Include UpperCase?");
+    var password = generatePassword(passwordLength, nums, specialCharacters, lowerCase, upperCase);
+    var passwordText = document.querySelector("#password");
+  } else {
+    alert("Please make sure length is between 8 - 128 characters.");
+  }
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
